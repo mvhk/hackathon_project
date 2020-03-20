639e3f5f08a0dba2a2c1b3a92437ec304adfe949
Venkata Harish Kumar Mamidi <harish.mamidi.kumar@gmail.com>
2020-03-20 15:43:32 +0530

    Created JSON object

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -3,30 +3,33 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
+import org.json.simple.JSONObject;
+
 public class ReadFile {
 	static BufferedReader br = null;
 	static int itr = 0;
+	static String some;
 
 	public static void main(String[] args) {
+		JSONObject obj = new JSONObject();
+
 		try {
 			String line;
 			br = new BufferedReader(new FileReader("C:\\Users\\haris\\Downloads\\sample-input.txt"));
 			while ((line = br.readLine()) != null) {
-				System.out.println(line);
-
 				StringTokenizer stringTokenizer = new StringTokenizer(line, " ");
 
 				while (stringTokenizer.hasMoreElements()) {
 
-					int x=0;
-					while(x<8) {
+					int x = 0;
+					while (x < 8) {
 						stringTokenizer.nextElement().toString();
 						x++;
 					}
-					
+
 //					required line
 					Double reqCPU = Double.parseDouble(stringTokenizer.nextElement().toString());
-					while(x<11) {
+					while (x < 11) {
 						stringTokenizer.nextElement().toString();
 						x++;
 					}
@@ -34,9 +37,14 @@ public class ReadFile {
 					StringBuilder sb = new StringBuilder();
 					sb.append(itr + "s" + reqCPU);
 					itr++;
+					some = itr + "s";
+					obj.put(some, reqCPU);
+
 					System.out.println(sb.toString());
 				}
+
 			}
+			System.out.println(obj);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {

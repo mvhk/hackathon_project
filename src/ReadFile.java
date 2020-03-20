32fb9840e0bba66db2f087c536f330c1db3cc9a6
Venkata Harish Kumar Mamidi <harish.mamidi.kumar@gmail.com>
2020-03-20 15:56:24 +0530

    JSON average and total added

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -7,8 +7,13 @@ import org.json.simple.JSONObject;
 
 public class ReadFile {
 	static BufferedReader br = null;
+//	iterator
 	static int itr = 0;
+// helper at adding to json
 	static String some;
+//	array for calculating the max and avg
+	static double[] arr = new double[1000];
+	static double total = 0;
 
 	public static void main(String[] args) {
 		JSONObject obj = new JSONObject();
@@ -20,7 +25,7 @@ public class ReadFile {
 				StringTokenizer stringTokenizer = new StringTokenizer(line, " ");
 
 				while (stringTokenizer.hasMoreElements()) {
-
+//iterating to required cpu value in a row
 					int x = 0;
 					while (x < 8) {
 						stringTokenizer.nextElement().toString();
@@ -34,16 +39,31 @@ public class ReadFile {
 						x++;
 					}
 
-					StringBuilder sb = new StringBuilder();
-					sb.append(itr + "s" + reqCPU);
+//					StringBuilder sb = new StringBuilder();
+//					sb.append(itr + "s" + reqCPU);
+//					iterator
+
 					itr++;
+//					pushing it to json
 					some = itr + "s";
 					obj.put(some, reqCPU);
-
-					System.out.println(sb.toString());
+					arr[itr] = reqCPU;
+//					System.out.println(sb.toString());
 				}
 
 			}
+			for (int i = 0; i < arr.length; i++) {
+				total = total + arr[i];
+			}
+			double average = total / arr.length;
+
+//			  System.out.println(total);
+//			  System.out.println(average);
+
+			obj.put("total", total);
+			obj.put("average", average);
+//			  
+//			printing the json
 			System.out.println(obj);
 		} catch (Exception e) {
 			e.printStackTrace();

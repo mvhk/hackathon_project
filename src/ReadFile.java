dbb46d28f8f0df7301882051369c77fdb3d787d9
Venkata Harish Kumar Mamidi <harish.mamidi.kumar@gmail.com>
2020-03-21 14:55:51 +0530

    Comments for Final code were added

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -13,43 +13,72 @@ import com.mysql.jdbc.PreparedStatement;
 
 public class ReadFile {
 	static BufferedReader br = null;
+
 //	iterator
 	static int itr = 0;
+
 // helper at adding to json
 	static String some;
+
 //	array for calculating the max and avg
 	static double[] arr = new double[1000];
+
+//	total for counting the average
 	static double total = 0;
+
+//	maximum value in the given input
 	static double max = 0;
 
+//	Initializing the connection
 	static Connection connection = null;
+
+//	Initializing the Statement
 	static Statement statement = null;
+
+//	Resultset
 	ResultSet rs = null;
+
+//	Database Initialization
 	static String databaseName = "HackPro";
 	static String url = "jdbc:mysql://localhost:3333/" + databaseName;
 	static String userName = "root";
 	static String passWord = "1234";
 
 	public static void main(String[] args) {
+
+//		Json object initialization
 		JSONObject obj = new JSONObject();
 
 		try {
+
+//			getting the connection from the server
 			connection = DriverManager.getConnection(url, userName, passWord);
 
 			System.out.println("Database connection successful!\n");
 
 			statement = connection.createStatement();
 
+//			Inserting the values into the table 
 			PreparedStatement pStmt = (PreparedStatement) connection
 					.prepareStatement("INSERT into table1 (transaction, avg, max) values(?,?,?)");
+
 			String line;
+
+//			getting the required file
 			br = new BufferedReader(new FileReader("C:\\Users\\haris\\Downloads\\sample-input.txt"));
+//			looping through the entire file
 			while ((line = br.readLine()) != null) {
+
+//				getting the values in the file based on the space separating them
 				StringTokenizer stringTokenizer = new StringTokenizer(line, " ");
+
+//				pushing the transaction name to the table for the particular row
 				pStmt.setString(1, "Transaction2");
+
 				while (stringTokenizer.hasMoreElements()) {
-//iterating to required cpu value in a row
+//					iterating to required CPU value in a row
 					int x = 0;
+//					looping until the required CPU data
 					while (x < 8) {
 						stringTokenizer.nextElement().toString();
 						x++;
@@ -57,8 +86,11 @@ public class ReadFile {
 
 //					required line
 					Double reqCPU = Double.parseDouble(stringTokenizer.nextElement().toString());
+
+//					changing the max value
 					if (max < reqCPU)
 						max = reqCPU;
+
 					while (x < 11) {
 						stringTokenizer.nextElement().toString();
 						x++;
@@ -69,12 +101,12 @@ public class ReadFile {
 //					iterator
 
 					itr++;
-					
+
 //					pushing it to json
 					some = itr + "s";
-					
+
 					obj.put(some, reqCPU);
-					
+
 					arr[itr] = reqCPU;
 //					System.out.println(sb.toString());
 				}
@@ -83,7 +115,7 @@ public class ReadFile {
 			for (int i = 0; i < arr.length; i++) {
 				total = total + arr[i];
 			}
-			
+
 			double average = total / arr.length;
 
 //			  System.out.println(total);
@@ -91,12 +123,12 @@ public class ReadFile {
 
 			obj.put("total", total);
 			obj.put("average", average);
-			
+
 //			pushing to db
 			pStmt.setDouble(2, average);
 			pStmt.setDouble(3, total);
 			pStmt.execute();
-//			  
+			  
 //			printing the json
 			System.out.println(obj);
 
@@ -104,6 +136,7 @@ public class ReadFile {
 			e.printStackTrace();
 		} finally {
 			try {
+				
 				if (br != null)
 					br.close();
 
